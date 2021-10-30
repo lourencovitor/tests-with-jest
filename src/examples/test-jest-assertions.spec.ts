eaa66bb7a1712be498a7f6455cb0a43e0c350c34
Vitor Lourenço <vitor.brother17@outlook.com.br>
2021-10-30 19:59:15 -0300

    test: create test assertions

---
+++ src/examples/test-jest-assertions.spec.ts
@@ -0,0 +1,36 @@
+describe('Primitive values', () => {
+  it('should test jest assestions', () => {
+    const number = 10;
+
+    expect(number).toBe(10)
+    expect(number).toEqual(10)
+
+    expect(number).not.toBeFalsy()
+    expect(number).toBeTruthy()
+
+    expect(number).toBeGreaterThan(9)
+    expect(number).toBeGreaterThanOrEqual(10)
+    expect(number).toBeLessThan(11)
+    expect(number).toBeLessThanOrEqual(10)
+
+    expect(number).toBeCloseTo(10.001)
+    expect(number).toBeCloseTo(9.996)
+
+    expect(number).not.toBeNull()
+
+    expect(number).toHaveProperty('toString')
+  })
+})
+
+describe('Objects', () => {
+  it('should test jest assestions with objects', () => {
+    const person = {name: 'John', age:20}
+    const anotherPerson = {...person}
+
+    expect(person).toEqual(anotherPerson)
+    expect(person).not.toHaveProperty('lastName')
+    expect(person).toHaveProperty('age', 20)
+
+    expect(person.name).toBe('John')
+  })
+})
